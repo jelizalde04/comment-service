79650c52f2980e6f2bf3945dfc90019d1cd4adec
Johana Elizalde <njelizalde@uce.edu.ec>
2025-03-02 17:49:40 -0500

    feat(model): 💬 add comment model

---
+++ list-comments-microservice/models/Comment.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace ListCommentsMicroservice\Models;
+
+class Comment
+{
+    public $id;
+    public $recipe_id;
+    public $user_id;
+    public $content;
+    public $created_at;
+}
