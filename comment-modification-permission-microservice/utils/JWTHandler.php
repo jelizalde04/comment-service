c666112403c6b2fda5294843942af02ff85aecc4
Johana Elizalde <njelizalde@uce.edu.ec>
2025-03-02 17:41:12 -0500

    feat(utils): 🔐 add JWT handler utility

---
+++ comment-modification-permission-microservice/utils/JWTHandler.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace CommentPermissionMicroservice\Utils;
+
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+
+class JWTHandler
+{
+    private static $key;
+
+    public static function setKey(string $key): void
+    {
+        self::$key = $key;
+    }
+
+    public static function generateJWT(int $userId): string
+    {
+        $payload = [
+            'user_id' => $userId,
+            'exp' => time() + 3600, // Token expires in 1 hour
+        ];
+
+        return JWT::encode($payload, self::$key, 'HS256');
+    }
+
+    public static function validateJWT(string $token): array
+    {
+        try {
+            return (array) JWT::decode($token, new Key(self::$key, 'HS256'));
+        } catch (\Exception $e) {
+            return [];
+        }
+    }
+}
